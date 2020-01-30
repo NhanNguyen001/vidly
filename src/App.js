c27d00421455c9a0bef64380cacad2e28483d252
NhanNguyen001 <nhan2151@gmail.com>
2020-01-30 16:25:21 +0700

    added Refs to login form

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/common/navBar';
+import LoginForm from './components/common/loginForm';
 
 import './App.css';
 
-const App = () => {
+const App = () => { 
   return (
     <React.Fragment>
       <NavBar />
       <main className='container'>
         <Switch>
-          <Route path='/movies/:id' component={MovieForm} />
+          <Route path='/login' component={LoginForm} />
           <Route path='/movies' component={Movies}></Route>
           <Route path='/customers' component={Customers}></Route>
           <Route path='/rentals' component={Rentals}></Route>
